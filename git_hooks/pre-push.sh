c3fa82a6fb8d65bd2494796592071c93d2e347f6
Colin Johnson <cojohnson@wayfair.com>
2017-08-16 12:00:11 -0400

    run tests before pushing

---
+++ git_hooks/pre-push.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+autopep=$(autopep8 -dr .)
+
+if [[ -z $autopep ]]
+then
+	echo "> PEP8 passed !"
+else
+	echo "> PEP8 DID NOT pass !"
+	echo "$autopep" | colordiff
+	exit 1
+fi
+
+`nosetests`
+
+if [[ $? != 0 ]]
+then
+	echo "> Tests DID NOT pass !"
+	exit 1
+else
+	echo "> Tests passed !"
+fi
+
+echo "Ready to commit !"
+exit 0
