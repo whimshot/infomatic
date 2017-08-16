ff1c8d7a7e27cd403fc725c250cce0517eeb4e23
Colin Johnson <colinj@gmail.com>
2017-08-16 08:26:52 -0400

    updated

--- weatherui.py
+++ weatherui.py
@@ -15,6 +15,20 @@ Config.set('graphics', 'height', 480)
 Window.size = (800, 480)
 
 
+class TodayTitle(Label):
+    """docstring for WeatherBox."""
+
+    def __init__(self, **kwargs):
+        """Setup that weatherbox."""
+        super().__init__(**kwargs)
+        try:
+            pass
+        except Exception as e:
+            raise
+        finally:
+            pass
+
+
 class WeatherLabel(Label):
     """docstring for WeatherBox."""
 
@@ -29,12 +43,37 @@ class WeatherLabel(Label):
             pass
 
 
+class TodayBox(BoxLayout):
+    """docstring for WeatherBox."""
+
+    def __init__(self, **kwargs):
+        """Setup that weatherbox."""
+        super().__init__(**kwargs)
+        tl = TodayTitle()
+        tl.text = "Today's Weather"
+        self.add_widget(tl)
+        w = weather.Weather()
+        pp = pprint.PrettyPrinter(indent=4)
+        foo = pp.pformat(w.today)
+        tw = WeatherLabel()
+        tw.text = foo
+        self.add_widget(tw)
+        try:
+            pass
+        except Exception as e:
+            raise
+        finally:
+            pass
+
+
 class WeatherBox(BoxLayout):
     """docstring for WeatherBox."""
 
     def __init__(self, **kwargs):
         """Setup that weatherbox."""
         super().__init__(**kwargs)
+        tb = TodayBox()
+        self.add_widget(tb)
         try:
             pass
         except Exception as e:
@@ -49,13 +88,7 @@ class WeatherApp(App):
     def build(self):
         """Build the app."""
         try:
-            w = weather.Weather()
-            pp = pprint.PrettyPrinter(indent=4)
-            foo = pp.pformat(w.today)
-            wl = WeatherLabel()
-            wl.text = foo
             wb = WeatherBox()
-            wb.add_widget(wl)
             return wb
         except Exception as e:
             raise
